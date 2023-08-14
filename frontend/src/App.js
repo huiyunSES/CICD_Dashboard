f3826f34f4761163a13e264afc58ea5c018d01c2
hui-yun <135854945+huiyunSES@users.noreply.github.com>
2023-08-14 16:16:42 -0700

    Add files via upload

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { useRoutes } from 'react-router-dom'
 import Dashboard from './pages/executive_summary/index'
 import SignIn from './pages/login/SignIn'
-import Admin from './pages/admin/admin';
+import Admin from './pages/admin/index';
 import MainLayout from './layouts/sidebar';
 import NotFoundPage from './pages/NotFoundPage'
 import Developer from './pages/developer';
