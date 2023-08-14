a95859b523da08173ef44cd52eb69d153af94058
hui-yun <135854945+huiyunSES@users.noreply.github.com>
2023-08-14 16:18:01 -0700

    Add files via upload

--- frontend/src/services/request.js
+++ frontend/src/services/request.js
@@ -1,8 +1,23 @@
+// for node.js connection
 
 const baseUrl = 'http://localhost:3003/api/v1'; //port has to match with NodeJS_api
 
 export const get = (url) => {
     return fetch(baseUrl + url).then((res) => {
+        return res.json()
+    }).catch((err) => {
+        console.log(err)
+    })
+};
+
+export const post = (url,data) => {
+    return fetch(baseUrl + url,{
+        method:'post',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then((res) => {
         return res.json()
     }).catch((err) => {
         console.log(err)
